3367d65aa6b514787b279416f943ebc7c11d35d9
李志权 <373765482@qq.com>
2021-03-03 14:30:18 +0800

    no message

---
+++ SDKQZNetworkOffline1.1.5/QZNOSDK.framework/Headers/KRWMBlock.h
@@ -0,0 +1,43 @@
+//
+//  KRWMBlock.h
+//  KRoadWaterMeter
+//
+//  Created by 远征 马 on 2017/5/5.
+//  Copyright © 2017年 马远征. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "KRWMObject.h"
+#import "KRWMDefine.h"
+
+#ifndef KRWMBlock_h
+#define KRWMBlock_h
+
+/**
+ @discussion 蓝牙搜索结果回调
+
+ @param retArray 外设数组
+ @param aPeripheral 当前扫描到的蓝牙外设
+ */
+typedef void (^KRWMPeriphralsHandler)(NSArray<CBPeripheral*> *retArray, CBPeripheral *aPeripheral);
+
+
+/**
+ @discussion 同步数据回调block
+
+ @param syncType 当前同步类型
+ @param data 同步的数据
+ */
+typedef void (^KRWMDataHandler)(KRSyncType syncType, NSData *data);
+
+
+/**
+ @discussion 同步结果回调
+
+ @param syncType 当前同步类型
+ @param state 同步结果响应状态
+ */
+typedef void (^KRWMResultHandler)(KRSyncType syncType, KRSyncResponseState state);
+
+#endif /* KRWMBlock_h */
---
+++ SDKQZNetworkOffline1.1.5/QZNOSDK.framework/Headers/KRWMUtils.h
@@ -0,0 +1,56 @@
+//
+//  KRWMUtils.h
+//  KRoadWaterMeter
+//
+//  Created by 远征 马 on 2017/5/5.
+//  Copyright © 2017年 马远征. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KRWMObject.h"
+#import "KRWMSetHeatPumpObject.h"
+#import "KRWMDefine.h"
+ 
+/**
+ @class KRWMUtils
+ @discussion 蓝牙数据解析工具
+ */
+@interface KRWMUtils : NSObject
+
+
+/**
+ @discussion 查询响应结果是否返回正确
+ 
+ @param data 响应数据
+ @return YES/NO
+ */
++ (BOOL)isSuccessResponseWithData:(NSData*)data;
+
+
+/**
+ @discussion 提取错误码,失败的时候才有响应码
+ 
+ @param data 响应数据
+ @return KRErrorCode
+ */
++ (KRErrorCode)getErrorCodeFromData:(NSData*)data;
+
+/// 十六进制字符串转NSData
+/// @param str 十六进制字符串
++ (NSData *)convertHexStrToData:(NSString *)str;
+
+/**
+ data转字符串
+
+ @param data 数据
+ @return 返回字符串
+ */
++ (NSString*)bytesStringWithData:(NSData*)data;
+/// 普通字符串转换为十六进制的。
+/// @param string 普通字符串
++ (NSString *)hexStringFromString:(NSString *)string;
+// 十六进制转换为普通字符串的。
++ (NSString *)stringFromHexString:(NSString *)hexString;
+
+@end
+
